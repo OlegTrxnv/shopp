3da58ccab75fc0aa4e0c2735b13bb23cfc52d856
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2020-11-04 14:13:38 +0200

    Implement products fetch from backend
    - HomeScreen and ProductScreen fetch data from /api/products
    - set up "proxy": "http://127.0.0.1:5000"
    - products.js removed

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
+import axios from "axios";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
+    <>
       <h1>Latest products</h1>
       <Row>
         {products.map((product) => (
@@ -14,7 +24,7 @@ const HomeScreen = () => {
           </Col>
         ))}
       </Row>
-    </div>
+    </>
   );
 };
 
