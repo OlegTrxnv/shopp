b470c5cf080f3755fe262846f0fd0137089b87f6
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2020-11-05 16:45:35 +0200

    Scaffold models
    - create orderModel, productModel, userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  // timestamps option tells Mongoose to automatically manage createdAt and updatedAt properties
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
