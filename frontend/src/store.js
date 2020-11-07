49f2bb0d01c143da60cb06d702495badfb88916e
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2020-11-07 15:21:59 +0200

    Redux setup in frontend
    - install redux-related dependencies
    - create and wire up empty store
    - enable redux devtools

---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+const initialState = {};
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
